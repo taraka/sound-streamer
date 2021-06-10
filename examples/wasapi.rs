3095b7d93b9876036ed4934b8fa5be78d139275b
Tom Rawcliffe <git@tomrawcliffe.co.uk>
2021-06-10 21:55:21 +0100

    Basic examples

---
+++ examples/wasapi.rs
@@ -0,0 +1,187 @@
+use std::collections::VecDeque;
+use std::error;
+use std::sync::mpsc;
+use std::thread;
+use wasapi::*;
+use windows::initialize_mta;
+
+#[macro_use]
+extern crate log;
+use simplelog::*;
+
+type Res<T> = Result<T, Box<dyn error::Error>>;
+
+// Playback loop, play samples received from channel
+fn playback_loop(rx_play: std::sync::mpsc::Receiver<Vec<u8>>) -> Res<()> {
+    let device = get_default_device(&Direction::Render)?;
+    let mut audio_client = device.get_iaudioclient()?;
+    let desired_format = WaveFormat::new(32, 32, &SampleType::Float, 44100, 2);
+
+    let blockalign = desired_format.get_blockalign();
+    debug!("Desired playback format: {:?}", desired_format);
+
+    let (def_time, min_time) = audio_client.get_periods()?;
+    debug!("default period {}, min period {}", def_time, min_time);
+
+    audio_client.initialize_client(
+        &desired_format,
+        min_time as i64,
+        &Direction::Render,
+        &ShareMode::Shared,
+        true,
+    )?;
+    debug!("initialized playback");
+
+    let h_event = audio_client.set_get_eventhandle()?;
+
+    let mut buffer_frame_count = audio_client.get_bufferframecount()?;
+
+    let render_client = audio_client.get_audiorenderclient()?;
+    let mut sample_queue: VecDeque<u8> = VecDeque::with_capacity(
+        100 * blockalign as usize * (1024 + 2 * buffer_frame_count as usize),
+    );
+    audio_client.start_stream()?;
+    loop {
+        buffer_frame_count = audio_client.get_available_space_in_frames()?;
+        trace!("New buffer frame count {}", buffer_frame_count);
+        while sample_queue.len() < (blockalign as usize * buffer_frame_count as usize) {
+            debug!("need more samples");
+            match rx_play.try_recv() {
+                Ok(chunk) => {
+                    trace!("got chunk");
+                    for element in chunk.iter() {
+                        sample_queue.push_back(*element);
+                    }
+                }
+                Err(mpsc::TryRecvError::Empty) => {
+                    warn!("no data, filling with zeros");
+                    for _ in 0..((blockalign as usize * buffer_frame_count as usize)
+                        - sample_queue.len())
+                    {
+                        sample_queue.push_back(0);
+                    }
+                }
+                Err(_) => {
+                    error!("Channel is closed");
+                    break;
+                }
+            }
+        }
+
+        trace!("write");
+        render_client.write_to_device_from_deque(
+            buffer_frame_count as usize,
+            blockalign as usize,
+            &mut sample_queue,
+        )?;
+        trace!("write ok");
+        if h_event.wait_for_event(100000).is_err() {
+            error!("error, stopping playback");
+            audio_client.stop_stream()?;
+            break;
+        }
+    }
+    Ok(())
+}
+
+// Capture loop, capture samples and send in chunks of "chunksize" frames to channel
+fn capture_loop(tx_capt: std::sync::mpsc::SyncSender<Vec<u8>>, chunksize: usize) -> Res<()> {
+    let device = get_default_device(&Direction::Render)?;
+    let mut audio_client = device.get_iaudioclient()?;
+
+    let desired_format = WaveFormat::new(32, 32, &SampleType::Float, 44100, 2);
+
+    let blockalign = desired_format.get_blockalign();
+    debug!("Desired capture format: {:?}", desired_format);
+
+    let (def_time, min_time) = audio_client.get_periods()?;
+    debug!("default period {}, min period {}", def_time, min_time);
+
+    audio_client.initialize_client(
+        &desired_format,
+        min_time as i64,
+        &Direction::Capture,
+        &ShareMode::Shared,
+        true,
+    )?;
+    debug!("initialized capture");
+
+    let h_event = audio_client.set_get_eventhandle()?;
+
+    let buffer_frame_count = audio_client.get_bufferframecount()?;
+
+    let render_client = audio_client.get_audiocaptureclient()?;
+    let mut sample_queue: VecDeque<u8> = VecDeque::with_capacity(
+        100 * blockalign as usize * (1024 + 2 * buffer_frame_count as usize),
+    );
+    audio_client.start_stream()?;
+    loop {
+        while sample_queue.len() > (blockalign as usize * chunksize as usize) {
+            debug!("pushing samples");
+            let mut chunk = vec![0u8; blockalign as usize * chunksize as usize];
+            for element in chunk.iter_mut() {
+                *element = sample_queue.pop_front().unwrap();
+            }
+            tx_capt.send(chunk)?;
+        }
+        trace!("capturing");
+        render_client.read_from_device_to_deque(blockalign as usize, &mut sample_queue)?;
+        if h_event.wait_for_event(1000000).is_err() {
+            error!("error, stopping capture");
+            audio_client.stop_stream()?;
+            break;
+        }
+    }
+    Ok(())
+}
+
+// Main loop
+fn main() -> Res<()> {
+    let _ = SimpleLogger::init(
+        LevelFilter::Trace,
+        ConfigBuilder::new()
+            .set_time_format_str("%H:%M:%S%.3f")
+            .build(),
+    );
+
+    initialize_mta()?;
+    let (tx_play, rx_play): (
+        std::sync::mpsc::SyncSender<Vec<u8>>,
+        std::sync::mpsc::Receiver<Vec<u8>>,
+    ) = mpsc::sync_channel(2);
+    let (tx_capt, rx_capt): (
+        std::sync::mpsc::SyncSender<Vec<u8>>,
+        std::sync::mpsc::Receiver<Vec<u8>>,
+    ) = mpsc::sync_channel(2);
+    let chunksize = 4096;
+
+    // Playback
+    let _handle = thread::Builder::new()
+        .name("Player".to_string())
+        .spawn(move || {
+            let result = playback_loop(rx_play);
+            if let Err(err) = result {
+                error!("Playback failed with error {}", err);
+            }
+        });
+
+    // Capture
+    let _handle = thread::Builder::new()
+        .name("Capture".to_string())
+        .spawn(move || {
+            let result = capture_loop(tx_capt, chunksize);
+            if let Err(err) = result {
+                error!("Capture failed with error {}", err);
+            }
+        });
+
+    loop {
+        match rx_capt.recv() {
+            Ok(chunk) => {
+                debug!("sending");
+                tx_play.send(chunk).unwrap();
+            }
+            Err(err) => error!("Some error {}", err),
+        }
+    }
+}
